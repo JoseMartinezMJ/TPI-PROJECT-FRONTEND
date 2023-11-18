f1ba5ade04b8cbca27a036179bcd93853c7644ae
Jose Martinez <JantonioMJ.2020@hotmail.com>
2023-11-18 09:16:08 -0600

    Delete de prueba

--- controllers/adminController.js
+++
@@ -1,8 +0,0 @@
-// controllers/adminController.js
-function renderAdminDashboard() {
-    loadAndRenderTemplate('views/templates/header.html', 'header-container');
-    loadAndRenderTemplate('views/templates/main.html', 'main-container');
-    loadAndRenderTemplate('views/templates/footer.html', 'footer-container');
-    loadAndRenderTemplate('views/admin/dashboard.html', 'main-container');
-}
-
