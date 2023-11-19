bda219791ea71c96752d1f1fc7b0a09fb72bc7c2
Jose Martinez <JantonioMJ.2020@hotmail.com>
2023-11-18 20:03:02 -0600

    Estructura modifcada
    Se esta trabajando el sidebar de la tienda

---
+++ PopBusrt/resources/views/PopBurst.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-----===== css =====----->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
+    <!-----===== Boxicons CSS =====----->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>PopBurst</title>
+</head>
+<body>
+    <!-- Inicio de estructura SideBar Menu -->
+    <nav class="sidebar">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <img src="{{ asset('img/logo.png') }}" alt="logo">
+                </span>
+                <div class="text header-text">
+                    <span class="nameshop"></span>
+                    <span class="typeshop"></span>
+                </div>
+            </div>
+
+        </header>
+    </nav>
+</body>
+</html>
