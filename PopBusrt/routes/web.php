6fd04b5d27a024d71846482aab9cdb9326ce0ec2
edrasl <edrasl@debian.myguest.virtualbox.org>
2023-11-22 17:55:58 -0600

    integracion de cambios

--- PopBusrt/routes/web.php
+++ PopBusrt/routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImagenController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\LoginController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,13 @@ Route::get('/PopBurst', function () {
 });
 // Establece una ruta GET para la URL '/almacenar-imagen', dirigida a la acción 'almacenarImagen' del controlador 'ImagenController
 Route::get('/almacenar-imagen', [ImagenController::class, 'almacenarImagen']);
+
+// Rutas para el controlador de inicio de sesión
+Route::get('/Login', [LoginController::class, 'showLoginForm'])->name('Login.show'); // Vista del formulario de inicio de sesión
+Route::post('/Login', [LoginController::class, 'login'])->name('Login.login'); // Procesar el formulario de inicio de sesión
+
+// Rutas para el registro
+Route::get('/Register', function () {
+    return view('Register'); // Modifica aquí el nombre de la vista
+})->name('registro.show'); // Vista del formulario de registro
+Route::post('/registro', [RegisterController::class, 'registrarUsuario'])->name('registro.registrar'); // Procesar el formulario de registro
